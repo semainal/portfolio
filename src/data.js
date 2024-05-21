e2364c356b2e13682e441a09dbea4d4df30ed01e
Sema İnal <71395118+semainal@users.noreply.github.com>
2024-05-21 18:13:54 +0300

    Update data.js

--- src/data.js
+++ src/data.js
@@ -4,25 +4,25 @@ export const products = [
      img: "https://mathematicsgames.netlify.app/mathicon.png",
      link: "https://mathematicsgames.netlify.app",
    },
-   {
-     id: 2,
-     img: "",
-     link: "https://mathematicsgames.netlify.app",
-   },
-   {
-     id: 3,
-     img: "",
-     link: "https://mathematicsgames.netlify.app",
-   },
-   {
-     id: 4,
-     img: "",
-     link: "https://mathematicsgames.netlify.app",
-   },
-   {
-     id: 5,
-     img: "",
-     link: "https://mathematicsgames.netlify.app",
-   },
+   // {
+   //   id: 2,
+   //   img: "",
+   //   link: "https://mathematicsgames.netlify.app",
+   // },
+   // {
+   //   id: 3,
+   //   img: "",
+   //   link: "https://mathematicsgames.netlify.app",
+   // },
+   // {
+   //   id: 4,
+   //   img: "",
+   //   link: "https://mathematicsgames.netlify.app",
+   // },
+   // {
+   //   id: 5,
+   //   img: "",
+   //   link: "https://mathematicsgames.netlify.app",
+   // },
    
  ];
