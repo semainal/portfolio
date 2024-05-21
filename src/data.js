af2b60b8a2ecb321e09a9ef75c71e617cca34f1a
Sema İnal <71395118+semainal@users.noreply.github.com>
2024-05-21 21:42:40 +0300

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,14 +1,16 @@
+
+
 export const products = [
    {
      id: 1,
      img: "https://mathematicsgames.netlify.app/mathicon.png",
      link: "https://mathematicsgames.netlify.app",
    },
-   // {
-   //   id: 2,
-   //   img: "",
-   //   link: "https://mathematicsgames.netlify.app",
-   // },
+   {
+     id: 2,
+     img: "",
+     link: "https://astrogame.netlify.app/",
+   },
    // {
    //   id: 3,
    //   img: "",
